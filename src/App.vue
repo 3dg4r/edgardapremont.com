0b9a7eedd6ddbb84840dd322accff57040fd0195
Edgar Dapremont <cafedap@gmail.com>
2020-07-27 14:16:12 -0500

    Initial site build

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script>
+import NavigationBar from '@/components/navigation-bar.vue';
+
+export default {
+  name: 'App',
+  components: {
+    NavigationBar,
+  },
+};
+</script>
+
+<template>
+  <div id="app">
+    <div v-cloak>
+      <NavigationBar></NavigationBar>
+      <router-view/>
+    </div>
+  </div>
+</template>
+
+<style>
+[v-cloak] { display: none; }
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <p>Edgar Dapremont</p>
+    <p>UI/UX Developer for Websites and Mobile Apps</p>
+    <p>New Orleans, LA</p>
+    <p>Remote Employee</p>
+    <ul>
+      <li>Javscript</li>
+      <li>
+        <ul>
+          <li>Vue</li>
+          <li>React</li>
+          <li>Angular</li>
+          <li>Node</li>
+          <li>jQuery</li>
+        </ul>
+      </li>
+      <li>Git</li>
+      <li>CSS</li>
+      <li>HTML5</li>
+      <li>REST APIs</li>
+    </ul>
+    <div>
+      <p>14 years experience working with front end technologies</p>
+      <p>Agile and Kanban environments</p>
+      <p>Responsive website and app development</p>
+      <p>Solo and team experience as a developer</p>
+      <p>User interfaces used by 1000s of site visitors</p>
+      <p>Contributed to two patents</p>
+    </div>
+  </div>
+</template>
